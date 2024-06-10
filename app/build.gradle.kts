db01bc5cc26c934e18aad80ec40db897263d9fdf
PROBLEMatic619 <uragan619@gmail.com>
2024-06-10 19:46:58 +0700

    Start project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 }
--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -3,98 +3,92 @@ package com.example.geoquiz
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
+    private val quizViewModel: QuizViewModel by
+    lazy {
+        ViewModelProviders.of(this).get(QuizViewModel::class.java)
+    }
+
+
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
     private lateinit var questionTextView: TextView
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans,
-            true),
-        Question(R.string.question_mideast,
-            false),
-        Question(R.string.question_africa,
-            false),
-        Question(R.string.question_americas
-            , true),
-        Question(R.string.question_asia,
-            true))
-    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG,
-            "onCreate(Bundle?) called")
+        Log.d(TAG, "onCreate(Bundle?) called")
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
-        nextButton = findViewById(R.id.next_button)
-        questionTextView = findViewById(R.id.question_text_view)
-        trueButton.setOnClickListener { view: View ->
 
-            checkAnswer(true)
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
-        falseButton.setOnClickListener { view: View ->
+
+        trueButton = findViewById(R.id.true_button)
+        trueButton.setOnClickListener{view: View ->
+            checkAnswer(true)
+        }
+
+        falseButton = findViewById(R.id.false_button)
+        falseButton.setOnClickListener{view: View ->
             checkAnswer(false)
         }
+
         nextButton = findViewById(R.id.next_button)
-        //questionTextView = findViewById(R.id.textViewQuestion)
+        questionTextView = findViewById(R.id.question_text_view)
 
         nextButton.setOnClickListener {
-            currentIndex = (currentIndex + 1) % questionBank.size
-            val questionTextResId = questionBank[currentIndex].textResId
-            questionTextView.setText(questionTextResId)
+            quizViewModel.moveToNext()
+            updateQuestion()
         }
-        val questionTextResId = questionBank[currentIndex].textResId
-        questionTextView.setText(questionTextResId)
-
+        updateQuestion()
     }
+
     override fun onStart() {
         super.onStart()
-        Log.d(TAG,
-            "onStart() called")
+        Log.d(TAG,"onStart() called")
     }
     override fun onResume() {
         super.onResume()
-        Log.d(TAG,
-            "onResume() called")
+        Log.d(TAG, "onResume() called")
     }
     override fun onPause() {
         super.onPause()
-        Log.d(TAG,
-            "onPause() called")
+        Log.d(TAG,"onPause() called")
     }
     override fun onStop() {
         super.onStop()
-        Log.d(TAG,
-            "onStop() called")
+        Log.d(TAG, "onStop() called")
     }
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(TAG,
-            "onDestroy() called")
+        Log.d(TAG,"onDestroy() called")
     }
 
     private fun updateQuestion() {
-        val questionTextResId = questionBank[currentIndex].textResId
+        val questionTextResId = quizViewModel.currentQuestionText
         questionTextView.setText(questionTextResId)
     }
 
     private fun checkAnswer(userAnswer:Boolean) {
-        val correctAnswer = questionBank[currentIndex].answer
+        val correctAnswer = quizViewModel.currentQuestionAnswer
         val messageResId = if (userAnswer == correctAnswer) {
             R.string.correct_toast
         } else {
@@ -103,12 +97,6 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, messageResId,
             Toast.LENGTH_SHORT)
             .show()
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
+
